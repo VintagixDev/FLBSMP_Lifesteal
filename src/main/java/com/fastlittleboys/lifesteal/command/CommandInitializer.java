126f6fbcf25e9e1215fc68478c3ffe9a80dadc51
Vintagix <noreply@github.com>
2026-01-08 22:30:34 +0100

    Cleaned the intends

--- src/main/java/com/fastlittleboys/lifesteal/command/CommandInitializer.java
+++ src/main/java/com/fastlittleboys/lifesteal/command/CommandInitializer.java
@@ -9,21 +9,21 @@ public class CommandInitializer {
     public static void initialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(
-                    Commands.literal("withdraw")
-                            // /withdraw
-                            .executes(context ->
-                                    WithdrawCommand.executeWithdrawCommand(context, 1)
+                Commands.literal("withdraw")
+                     // /withdraw
+                     .executes(context ->
+                         WithdrawCommand.executeWithdrawCommand(context, 1)
+                     )
+                     // /withdraw <amount>
+                     .then(Commands.argument("amount", IntegerArgumentType.integer(1))
+                        .executes(context ->
+                            WithdrawCommand.executeWithdrawCommand(context,
+                                IntegerArgumentType.getInteger(context, "amount")
                             )
-                            // /withdraw <amount>
-                            .then(Commands.argument("amount", IntegerArgumentType.integer(1))
-                                    .executes(context ->
-                                            WithdrawCommand.executeWithdrawCommand(
-                                                    context,
-                                                    IntegerArgumentType.getInteger(context, "amount")
-                                            )
-                                    )
-                            )
+                        )
+                     )
             );
+
         });
     }
 }
