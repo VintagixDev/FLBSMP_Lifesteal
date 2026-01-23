6770f8e209e1b398649b67568305aafb2615c50a
Vintagix <noreply@github.com>
2026-01-23 16:16:49 +0100

    Removed unused imports

--- src/main/java/com/fastlittleboys/lifesteal/item/ReviveItem.java
+++ src/main/java/com/fastlittleboys/lifesteal/item/ReviveItem.java
@@ -1,28 +1,14 @@
 package com.fastlittleboys.lifesteal.item;
 
-import com.fastlittleboys.lifesteal.Lifesteal;
-import com.fastlittleboys.lifesteal.component.ModComponents;
 import com.fastlittleboys.lifesteal.component.ReviveMenu;
-import com.fastlittleboys.lifesteal.event.ServerInstance;
-import net.minecraft.ChatFormatting;
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-
-import net.minecraft.world.item.component.ResolvableProfile;
 import net.minecraft.world.level.Level;
 import org.jspecify.annotations.NonNull;
 
-import java.util.Objects;
-import java.util.Set;
-import java.util.UUID;
 
 public class ReviveItem extends Item {
     public ReviveItem(Properties settings) {
